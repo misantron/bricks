b0eb897c838581ddf902e65daf8bb2894c35b88a
misantron <misantron@gmail.com>
2015-11-26 00:00:05 +0300

    Base test case class added, PHPUnit version update

---
+++ tests/BaseTestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Bricks\Tests;
+
+/**
+ * Class BaseTestCase
+ * @package Bricks\Tests
+ */
+class BaseTestCase extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Call protected class method using reflection
+     *
+     * @param string $obj
+     * @param string $name
+     * @param array $args
+     * @return mixed
+     */
+    protected static function callMethod($obj, $name, $args = array())
+    {
+        $class = new \ReflectionClass($obj);
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method->invokeArgs(null, $args);
+    }
+}
